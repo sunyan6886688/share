d80bac66f1b3cc5709dc4dc031cee90545df95d4
sunyan6886688@163.com <sunyan6886688@163.com>
2019-06-03 17:06:17 +0800

    登录

--- yyzlpt_mall/src/pages/WechatShare/User/Login.js
+++ yyzlpt_mall/src/pages/WechatShare/User/Login.js
@@ -1,31 +1,68 @@
-
 import ReactDOM from 'react-dom';
 import React, { PureComponent } from 'react';
 import styles from './Login.less';
 
 class Index extends PureComponent {
+  state = {
+    IDcard: '',
+    PhoneCode: '',
+    clickFlage: true,
+    tips: '获取动态码',
+  };
+  changeIDcard = e => {
+    this.setState({
+      IDcard: e.target.value,
+      clickFlage: true,
+      tips: '获取动态码',
+    });
+  };
+  changePhoneCode = e => {
+    this.setState({
+      PhoneCode: e.target.value,
+    });
+  };
+  getPhoneCode = () => {
+    if (this.state.IDcard.length <= 8) {
+      return false;
+    }
+    this.setState({
+      clickFlage: false,
+      tips: '59s后重发',
+    });
+  };
   render() {
+    const buttonStyles =
+      this.state.IDcard && this.state.PhoneCode ? styles.buttonActive : styles.button;
+    const spanStyles =
+      this.state.IDcard.length > 8 && this.state.clickFlage
+        ? styles.spanActive
+        : styles.spanNoActive;
     return (
       <div className={styles.LoginBody}>
         <div>
-          <p className={styles.header}>动态码登录</p>
+          <p className={styles.header}>
+            <span className={styles.spanActive}>动态码登录</span>
+            <span>密码登录</span>
+          </p>
           <ul>
-            <li className={styles.userName}
-            ><input placeholder='身份证/台胞证/港澳证'></input>
+            <li className={styles.userName}>
+              <input placeholder="身份证/台胞证/港澳证" onChange={this.changeIDcard} />
             </li>
             <li className={styles.code}>
-              <input placeholder='短信动态码'></input>
-              <span>获取动态码</span>
+              <input placeholder="短信动态码" onChange={this.changePhoneCode} />
+              <span className={spanStyles} onClick={this.getPhoneCode}>
+                {this.state.tips}
+              </span>
             </li>
+            <div className={styles.tips}>
+              <p>动态码已发送到手机138****0571</p>
+              <b>没有账号？点此注册</b>
+            </div>
           </ul>
-          <div className={styles.tips}>
-            <p>动态码已发送到手机138****0571</p>
-            <b>没有账号？点此注册</b>
-          </div>
-          <div className={styles.button}>登录</div>
+
+          <div className={buttonStyles}>登录</div>
         </div>
       </div>
-
     );
   }
 }
