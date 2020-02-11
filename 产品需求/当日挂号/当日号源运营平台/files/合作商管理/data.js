5d3a9bf4b4afc1b8139580d9ddb1af0ec3a243a1
liuxiaozhi <baidianke@gmail.com>
2020-02-11 14:35:03 +0800

    暂时用的文件

--- 产品需求/当日挂号/当日号源运营平台/files/合作商管理/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,H,I,H,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(V,_(W,X,Y,[_(W,Z,ba,g,bb,[_(bc,bd,W,be,bf,_(bg,bh,bi,[]))])])),bj,_(bk,[])),bl,_(),bm,_());}; 
-var b="url",c="合作商管理.html",d="generationDate",e=new Date(1560753309342.26),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="sfz",l="sfz_n",m="login",n="page",o="packageId",p="81073474f78147578d1c5abeefce5ac7",q="type",r="Axure:Page",s="name",t="合作商管理",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFF6F7F9,F="image",G="imageHorizontalAlignment",H="near",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="onLoad",W="description",X="页面载入时",Y="cases",Z="Case 1",ba="isNewIfGroup",bb="actions",bc="action",bd="setFunction",be="设置 选中状态于 Unidentified = &quot;true&quot;",bf="expr",bg="exprType",bh="block",bi="subExprs",bj="diagram",bk="objects",bl="masters",bm="objectPaths";
-return _creator();
-})());
--- 产品需求/当日挂号/当日挂号原型/files/运营管理/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j,k,l,m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,H,I,H,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
-var b="url",c="运营管理.html",d="generationDate",e=new Date(1560753376102.57),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="sfz",l="sfz_n",m="login",n="page",o="packageId",p="b0407473ae194b089fbe6bb7af90cb66",q="type",r="Axure:Page",s="name",t="运营管理",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="center",A="fill",B="fillType",C="solid",D="color",E=0xFFF6F7F9,F="image",G="imageHorizontalAlignment",H="near",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
-return _creator();
-})());
